69b0224318891f6862a2a4edb35747aff9d9c064
Stefan C <admin@exzocoin.com>
2023-09-22 04:51:30 -0400

    Update addresses.py

--- vestings/addresses.py
+++ vestings/addresses.py
@@ -17,19 +17,19 @@ from vesting import VestingType
 def get_airdrop_addresses(chain_id: int) -> Dict[str, Dict[int, HexBytes]]:
     return {
         VestingType.USER: {
-            1: MAINNET_USER_AIRDROP_ADDRESS,
+            1229: MAINNET_USER_AIRDROP_ADDRESS,
             5: GOERLI_USER_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.USER_V2: {
-            1: MAINNET_USER_V2_AIRDROP_ADDRESS,
+            1229: MAINNET_USER_V2_AIRDROP_ADDRESS,
             5: GOERLI_USER_V2_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.ECOSYSTEM: {
-            1: MAINNET_ECOSYSTEM_AIRDROP_ADDRESS,
+            1229: MAINNET_ECOSYSTEM_AIRDROP_ADDRESS,
             5: GOERLI_ECOSYSTEM_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.INVESTOR: {
-            1: MAINNET_INVESTOR_VESTING_POOL_ADDRESS,
+            1229: MAINNET_INVESTOR_VESTING_POOL_ADDRESS,
             5: GOERLI_INVESTOR_VESTING_POOL_ADDRESS,
         }[chain_id],
     }
